7b522094e904379fe8b98d6ec6b35e404feeb1b9
hyunjeongPark <GuoHJP@gmail.com>
2021-04-28 20:14:32 +0900

    Habit 컴포넌트 만들기

---
+++ habit-tracker/src/components/habit.jsx
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+
+class Habit extends Component {
+    render() {  
+        return(
+            <li className='habit'>
+                <span className='habit-name'>Reading</span>
+                <span className='habit-count'>8</span>
+                <button>
+                    <i className='fas fa-plus-square'></i>
+                </button>
+                <button>
+                    <i className='fas fa-minus-square'></i>
+                </button>
+            </li>
+        )
+    }
+}
+
+export default Habit;
