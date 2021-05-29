53ce6a16a832e79d407a60fa90e9ce2aff54b5a2
hyunjeongPark <GuoHJP@gmail.com>
2021-05-29 22:43:15 +0900

    env파일, Google로그인, Github로그인 모달창 구현

--- card-maker/src/App.jsx
+++ card-maker/src/App.jsx
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login/login';
 import {BrowserRouter} from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-    </BrowserRouter>
-  );
+function App({authService}) {
+  return <Login authService={authService}/>;
 }
 
 export default App;
---
+++ card-maker/src/components/login/login.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+const Login = ({authService}) => {
+    const onLogin = (e) => {
+        authService//
+        .login(e.currentTarget.textContent)//
+        .then(console.log);
+    }
+    return(
+        <section>
+            <Header />
+            <section>
+                <h1>Login</h1>
+                <ul>
+                    <li>
+                        <button onClick={onLogin}>Google</button>
+                    </li> 
+                    <li>
+                        <button onClick={onLogin}>Github</button>
+                    </li>
+                </ul>
+            </section>
+            <Footer />
+        </section>
+    );
+};
+
+export default Login;
