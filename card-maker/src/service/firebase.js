d6a612e0896138c3b5ed6dd66eaaa20e4a86d017
hyunjeongPark <GuoHJP@gmail.com>
2021-05-29 19:48:29 +0900

    firebase 추가

---
+++ card-maker/src/service/firebase.js
@@ -0,0 +1,7 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyDjgvpv53CrArfLzILL8qreQCCt3K7GN5U",
+    authDomain: "business-card-maker-8711f.firebaseapp.com",
+    projectId: "business-card-maker-8711f",
+    databaseURL: "https://business-card-maker-8711f-default-rtdb.firebaseio.com",
+};
+firebase.initializeApp(firebaseConfig);
